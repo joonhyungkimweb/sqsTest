f0b4d0f059082bd42bab7f0650336be2678c7ab4
nick <nicolas.ai.nomis@gmail.com>
2022-10-04 13:25:11 +0900

    [feat] TrainingParams 순서 수정 및 learningRate 추가

--- src/Modules/MessagePasers.ts
+++ src/Modules/MessagePasers.ts
@@ -1,22 +1,23 @@
 import { getParams } from './DB';
 interface TrainingParams {
   instanceId: string;
+  trainingSeq: string;
   userId: string;
+  projectName: string;
   modelName: string;
   datasetPath: string;
   modelPath: string;
   weightsPath: string;
-  xColumns: number[];
-  yColumns: number[];
   optimizer: string;
   loss: string;
   metrics: string;
-  batchSize: number;
+  learningRate: number;
   epochs: number;
+  batchSize: number;
   shuffle: boolean;
   validationSplit: number;
-  trainingSeq: string;
-  projectName: string;
+  xColumns: number[];
+  yColumns: number[];
 }
 
 export const getTrainingParams = async (targetId: string): Promise<TrainingParams> => {
@@ -24,43 +25,45 @@ export const getTrainingParams = async (targetId: string): Promise<TrainingParam
   if (Items == null) throw new Error('no Message');
   const [
     {
-      modelPath: { S: modelPath },
+      instanceId: { S: instanceId },
+      trainingSeq: { S: trainingSeq },
+      userId: { S: userId },
+      projectName: { S: projectName },
+      modelName: { S: modelName },
       datasetPath: { S: datasetPath },
+      modelPath: { S: modelPath },
+      weightsPath: { S: weightsPath },
+      optimizer: { S: optimizer },
+      loss: { S: loss },
+      metrics: { S: metrics },
+      learningRate: { N: learningRate },
+      epochs: { N: epochs },
+      batchSize: { N: batchSize },
+      shuffle: { BOOL: shuffle },
+      validationSplit: { N: validationSplit },
       xColumns: { L: xColumns },
       yColumns: { L: yColumns },
-      loss: { S: loss },
-      weightsPath: { S: weightsPath },
-      batchSize: { N: batchSize },
-      userId: { S: userId },
-      epochs: { N: epochs },
-      validationSplit: { N: validationSplit },
-      instanceId: { S: instanceId },
-      metrics: { S: metrics },
-      modelName: { S: modelName },
-      optimizer: { S: optimizer },
-      shuffle: { BOOL: shuffle },
-      trainingSeq: { S: trainingSeq },
-      projectName: { S: projectName },
     },
   ] = Items;
 
   return {
     instanceId: instanceId!,
+    trainingSeq: trainingSeq!,
     userId: userId!,
+    projectName: projectName!,
     modelName: modelName!,
     datasetPath: datasetPath!,
     modelPath: modelPath!,
     weightsPath: weightsPath!,
-    xColumns: xColumns!.map(({ N }) => +N!),
-    yColumns: yColumns!.map(({ N }) => +N!),
     optimizer: optimizer!,
     loss: loss!,
     metrics: metrics!,
-    batchSize: +batchSize!,
+    learningRate: +learningRate!,
     epochs: +epochs!,
+    batchSize: +batchSize!,
     shuffle: shuffle!,
     validationSplit: +validationSplit!,
-    trainingSeq: trainingSeq!,
-    projectName: projectName!,
+    xColumns: xColumns!.map(({ N }) => +N!),
+    yColumns: yColumns!.map(({ N }) => +N!),
   };
 };
