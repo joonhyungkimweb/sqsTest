84fc2cb6d6c87d7373b8988c166ead14a370e077
nick <nicolas.ai.nomis@gmail.com>
2022-10-04 13:06:43 +0900

    [feat] compileOptimizer 추가

--- src/Modules/ModelTrainer.ts
+++ src/Modules/ModelTrainer.ts
@@ -1,4 +1,22 @@
-import { LayersModel, ModelCompileArgs, ModelFitArgs, Tensor } from '@tensorflow/tfjs-node-gpu';
+import {
+  LayersModel,
+  ModelCompileArgs,
+  ModelFitArgs,
+  Tensor,
+  train,
+} from '@tensorflow/tfjs-node-gpu';
+
+const optimizers = {
+  Adam: train.adam,
+  SGD: train.sgd,
+  Adagrad: train.adagrad,
+  Adadelta: train.adadelta,
+  Adamax: train.adamax,
+  RMSProp: train.rmsprop,
+};
+
+export const compileOptimizer = (optimzer: keyof typeof optimizers, learningRate: number) =>
+  optimizers[optimzer](learningRate);
 
 export const trainModel = async (
   {
